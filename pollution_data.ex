3720c546c31a6e8dd8d0a350f0dac704cde2955e
pswedrak <piotrekswedrak@gmail.com>
2018-05-26 23:24:51 +0200

    Elixir

--- pollution_data.ex
+++ pollution_data.ex
@@ -21,9 +21,40 @@ defmodule PollutionData do
     record_map = Map.put(record_map, :pollutionLevel, pollutionLevel)
   end
 
-  def identifyStations(name) do
-      list = importLinesFromCSV(name)
-      Enum.reduce(list, [], fn line, acc -> [parseLine(line) | acc] end)
+  def identifyStations(list) do
+      mapa = list |> Enum.reduce(%{}, fn line, acc -> Map.put(acc, line[:location], []) end)
+      Map.keys(mapa)
+  end
+
+  def startServer() do
+    spawn fn -> :pollution_gen_server.start() end
+  end
+
+  def stopServer() do
+    :pollution_gen_server.stop()
+  end
+
+  def loadStations(list) do
+    stations = identifyStations(list)
+    Enum.map(stations, fn {x, y} -> :pollution_gen_server.addStation("station_#{x}_#{y}", {x, y}) end)
+  end
+
+  def loadRecords(list) do
+    Enum.map(list, fn record -> :pollution_gen_server.addValue(record[:location], record[:datetime], "PM10", record[:pollutionLevel]) end)
+  end
+
+  def test(name) do
+    startServer()
+
+    file = importLinesFromCSV(name)
+    list = Enum.reduce(file, [], fn line, acc -> [parseLine(line) | acc] end)
+
+    stations_time = fn -> loadStations(list) end |> :timer.tc |> elem(0)
+    records_time = fn -> loadRecords(list) end |> :timer.tc |> elem(0)
+    #dailymean = fn -> :pollution_gen_server.getDailyMean({20.06, 49.986}, "PM10") end |> :timer.tc
+    stopServer()
+    {stations_time / 1000000, records_time / 1000000}
+    #dailymean
   end
 
 end
